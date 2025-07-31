4295c1019008d9ff3f5f8892994f73ee86cd9862
gyuhong <jagijudo@gmail.com>
2025-07-31 09:53:06 +0900

    3번 입니다.

--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,12 @@
 import pytest
 
+from calc import Calc
+
 
 def test_sample():
     assert 1 ==1
+
+def test_getZegop():
+    calc = Calc()
+    for num in range(100):
+        assert calc.getZegop(num) == num * num
