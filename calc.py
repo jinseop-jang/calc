709de5640829ad3da1006cdbd7e56b34a2fb4430
soiscow <thgus_so@naver.com>
2025-07-31 10:19:34 +0900

    feature4

--- calc.py
+++ calc.py
@@ -1,3 +1,3 @@
 class Calc:
-    pass
-
+    def get_minus(self, num1, num2):
+        return  num1 - num2
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,9 @@
 import pytest
 
+from calc import Calc
 
-def test_sample():
-    assert 1 ==1
+
+def test_get_minus():
+    calc = Calc()
+    assert calc.get_minus(7, 2) == 5
+    assert calc.get_minus(2, 7) == -5
