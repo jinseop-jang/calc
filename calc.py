1e89b8e51b661edc718be149097b1f23a092ff3e
jinseop <jjs0110zz@naver.com>
2025-07-31 10:14:32 +0900

    [feature] feature5 getDivide divide 0 exception

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
 class Calc:
-    def getDivide(self,param1,param2):
-        return 2
-
+    def getDivide(self,param1, param2):
+        try:
+            return round(param1/param2, 2)
+        except ZeroDivisionError:
+            return 0.0
--- test_calc.py
+++ test_calc.py
@@ -10,5 +10,10 @@ def test_sample():
 def test_getDivide():
     calc = Calc()
     assert calc.getDivide(4,2) == 2
+    assert calc.getDivide(5,3) == 1.67
+
+def test_getDivide_divide_by_0():
+    calc = Calc()
+    assert calc.getDivide(5,0)== 0.0
 
 
